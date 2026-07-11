4fc4d726311b2fc05b658f97375d38f6572064c4
Karan Singh <singh.karangkp@gmail.com>
2026-07-12 00:29:18 +0530

    feat: add IC lookup by name and number

--- utils/lookup.py
+++ utils/lookup.py
@@ -1,17 +1,18 @@
 import sqlite3
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QLineEdit, QWidget, QGridLayout
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QWidget, QGridLayout, QListWidget, QListWidgetItem
 from utils.message import message
-from pages.ic_setup import ICSetup
 
 class search(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Lookup')
-        self.setGeometry(760, 480, 500, 200)
+        self.setGeometry(660, 380, 700, 450)
         self.initUI()
 
     def initUI(self):
         self.searchbar = QLineEdit()
+        self.searchbar.setPlaceholderText('Enter the  IC Name')
         searchbutton = QPushButton('Search')
         searchbutton.clicked.connect(self.see)
 
@@ -19,9 +20,12 @@ class search(QMainWindow):
         self.setCentralWidget(central_widget)
 
         grid = QGridLayout()
+        
+        self.listwidget = QListWidget()
 
         grid.addWidget(self.searchbar, 0, 0)
         grid.addWidget(searchbutton, 1, 0)
+        grid.addWidget(self.listwidget, 2, 0)
 
         central_widget.setLayout(grid)
 
@@ -31,54 +35,25 @@ class search(QMainWindow):
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
 
-        cursor.execute(f'''
-                       select * from ic_master where ic_name='{entry}'
+        cursor.execute('''
+                       select ic_no, ic_name from ic_master where ic_name like ?
 
-                        ''')
+                        ''', ('%' + entry + '%', ))
         
-        row = cursor.fetchone()
+        row = cursor.fetchall()
+        
+        
+        for opt in row:
+            
+            item = QListWidgetItem(f'{opt['ic_no']} - {opt['ic_name']}')
+            self.listwidget.addItem(item)
+        
+        
         if row is None:
             self.msg = message('Entry not found')
             self.msg.show()
             
             self.close()
         else:
-            print(tuple(row))
-            self.ic_window = ICSetup('A:Amend')
-            self.ic_window.ic_number_text.setText(str(row["ic_no"]))
-            self.ic_window.ic_name_text.setText(row["ic_name"])
-            self.ic_window.roledropdown.setCurrentText(row["role"])
-            self.ic_window.city_text.setText(row["city"])
-            self.ic_window.department_text.setText(row["department"])
-            self.ic_window.bank_text.setText(row["bank"])
-            self.ic_window.statusdropdown.setCurrentText(row["status"])
-            self.ic_window.pin_code_text.setText(row["pin_code"])
-            self.ic_window.account_no_text.setText(str(row["account_no"]))
-            self.ic_window.pan_no_text.setText(row["pan_no"])
-            self.ic_window.gst_no_text.setText(row["gst_no"])
-            self.ic_window.lei_no_text.setText(row["lei_no"])
-            self.ic_window.branch_text.setText(row["branch"])
-            self.ic_window.ifsc_code_text.setText(row["ifsc_code"])
-
-            self.ic_window.address1_text.setText(row["address1"])
-            self.ic_window.address2_text.setText(row["address2"])
-            self.ic_window.address3_text.setText(row["address3"])
-
-            self.ic_window.contact_name1_text.setText(row["name1"])
-            self.ic_window.contact_name2_text.setText(row["name2"])
-            self.ic_window.contact_name3_text.setText(row["name3"])
-
-            self.ic_window.designation1dropdown.setCurrentText(row["designation1"])
-            self.ic_window.designation2dropdown.setCurrentText(row["designation2"])
-            self.ic_window.designation3dropdown.setCurrentText(row["designation3"])
-
-            self.ic_window.phone1_text.setText(row["phone1"])
-            self.ic_window.phone2_text.setText(row["phone2"])
-            self.ic_window.phone3_text.setText(row["phone3"])
-
-            self.ic_window.email1_text.setText(row["email1"])
-            self.ic_window.email2_text.setText(row["email2"])
-            self.ic_window.email3_text.setText(row["email3"])
-            self.ic_window.show()
-            self.close()
-
+            pass
+        
