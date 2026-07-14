37c671b88cf52694cfa511a254938b03aa01b5cb
Karan Singh <singh.karangkp@gmail.com>
2026-07-14 19:16:59 +0530

    chore: removed unused imports

--- main_.py
+++ main_.py
@@ -1,14 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QGridLayout, QLineEdit, QTextEdit, QComboBox, QPushButton, QDateEdit
-from PyQt5.QtCore import QDate
-from utils.lookup import search
-from utils.search import search_
-from pages.ic_setup import ICSetup
+from PyQt5.QtWidgets import QApplication
 from pages.main_prompt_screen import MainWindow
 
 
-    
-    
 def main():
     app = QApplication([])
     with open("styles/dark.qss", "r") as file:
